9c84b4fd985953c0ea98dc2decb8fce15d9c6402
Colin Ronayne <cronayne@gmail.com>
2018-11-27 16:05:08 +0100

    Added Post Logic

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class PostListComponent {
   posts = [
-    {title: 'First Post', content: 'This is the first post\'s content'},
-    {title: 'Second Post', content: 'This is the second post\'s content'},
-    {title: 'Third Post', content: 'This is the third post\'s content'},
+    // {title: 'First Post', content: 'This is the first post\'s content'},
+    // {title: 'Second Post', content: 'This is the second post\'s content'},
+    // {title: 'Third Post', content: 'This is the third post\'s content'},
   ];
 }
