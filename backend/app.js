07648231af4bcfe5950d6652c24033faa83aa2e3
Colin Ronayne <cronayne@gmail.com>
2018-12-05 16:56:19 +0100

    Added connect to localhost and Retrieving and Deleteing a Post

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const Post = require('./models/post');
 
 const app = express();
+
+//MongoDB Atlas
+//mongoose.connect('mongodb+srv://cronayne:GtQFeTwEPgFwGUDA@nodejs-f1sha.mongodb.net/node_js?retryWrites=true', { useNewUrlParser: true })
+//Localhost
+mongoose.connect('mongodb://localhost:27017/node_js')
+  .then(() => {
+    console.log('Connected to the Database');
+  })
+  .catch(err => console.log('Mongo connection error', err))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +34,7 @@ app.post('/api/posts', (req, res, next) =>  {
     title: req.body.title,
     content: req.body.content
   });
+  post.save();
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
@@ -31,21 +42,18 @@ app.post('/api/posts', (req, res, next) =>  {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fasfdsfdsfs',
-      title: 'First serve-side post',
-      content: 'Woo look at me, I\'m from the server'
-    },
-    {
-      id: 'lksdfdfjkldsf',
-      title: 'Second serve-side post',
-      content: 'Woo look at me, I\'m from the server too!!!!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted!"});
   });
 });
 
