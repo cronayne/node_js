547996cb32ecbe739fb54be77eb5479a7e94bcbc
Colin Ronayne <cronayne@gmail.com>
2018-12-07 16:53:03 +0100

    Setup Initial Pagination Settings

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { PageEvent } from '@angular/material';
 
 import { Post } from '../posts.model';
 import { PostsService } from '../posts.service';
@@ -12,6 +13,9 @@ import { PostsService } from '../posts.service';
 export class PostListComponent implements OnInit, OnDestroy {
  posts: Post[] = [];
  isLoading = false;
+ totalPosts = 10;
+ postsPerPage = 2;
+ pageSizeOptions = [2, 5, 10, 20];
  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -24,6 +28,10 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.posts = posts;
       });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   onDelete(postId: string) {
