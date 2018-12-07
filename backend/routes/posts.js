8bd8cc3f5c94dc2e57ca992538fc8a5586eae9b1
Colin Ronayne <cronayne@gmail.com>
2018-12-07 15:56:06 +0100

    Made image visable to user

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -27,19 +27,24 @@ const storage = multer.diskStorage({
       .split(' ')
       .join('-');
     const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '-' + ext);
+    cb(null, name + '-' + Date.now() + '.' + ext);
   }
 });
 
 router.post('', multer({storage: storage}).single('image'), (req, res, next) =>  {
+  const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url + '/images/' + req.file.filename
   });
   post.save().then(createdPost => {
     res.status(201).json({
       message: 'Post added successfully',
-      postId: createdPost._id
+      post: {
+        ...createdPost,
+        id: createdPost._id,
+      }
     });
   });
 });
