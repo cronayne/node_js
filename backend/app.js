63f43ae358c297622a979ab857f494fc9a92665b
Colin Ronayne <cronayne@gmail.com>
2018-12-10 15:34:51 +0100

    Added Login Backend code

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const postsRoutes = require("./routes/posts")
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/users");
+
 const app = express();
 
 //MongoDB Atlas
@@ -31,5 +33,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
