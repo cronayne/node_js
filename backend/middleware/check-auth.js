dad0868a9e417a8422c7accae45a212655de95e4
Colin Ronayne <cronayne@gmail.com>
2018-12-11 17:28:52 +0100

    Added UserId to Posts

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "insert_a_long_string_here");
+    const decodedToken = jwt.verify(token, "insert_a_long_string_here");
+    req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch (error) {
     res.status(401).json({ message: "Auth failed" });
